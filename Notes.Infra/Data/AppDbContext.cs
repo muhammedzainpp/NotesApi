7a9dc686f238e39720de1e750958079b6d5e0c60
Shameem <shameem>
2022-09-10 20:07:13 +0530

    -Added Note seeder -Pull savechanges to IAppDbcontext -Fixed CORS

--- Notes.Infra/Data/AppDbContext.cs
+++ Notes.Infra/Data/AppDbContext.cs
@@ -16,7 +16,20 @@ public class AppDbContext : DbContext, IAppDbContext
     public DbSet<Note> Notes { get; set; } = default!;
     public DbSet<Label> Labels { get ; set ; } = default!;
 
+    public override int SaveChanges()
+    {
+        OnSave();
+        return base.SaveChanges();
+    }
+
     public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        OnSave();
+
+        return base.SaveChangesAsync(cancellationToken);
+    }
+
+    private void OnSave()
     {
         var now = DateTime.UtcNow;
 
@@ -46,8 +59,5 @@ public class AppDbContext : DbContext, IAppDbContext
                 }
             }
         }
-
-        return base.SaveChangesAsync(cancellationToken);
     }
-
 }
