e756fc8f0e4d72d6c277fbfd334ded218cb5b717
Shameem <shameem>
2022-09-23 22:30:34 +0530

    -Added Domain Exceptions

---
+++ Domain/Exceptions/NoteNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Exceptions;
+
+public class NoteNotFoundException : NotFoundException
+{
+    public NoteNotFoundException(int noteId)
+        : base($"The note with the identifier {noteId} was not found.")
+    {
+    }
+
+}
